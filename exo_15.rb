cf9d0291ee28158df830d6a3110be03a5f76e39f
Septentrion50 <erwan56lecorre@gmail.com>
2021-04-12 18:34:58 +0200

    bugFix sur exos 15, 16, et 17

--- exo_15.rb
+++ exo_15.rb
@@ -1,6 +1,12 @@
 puts "Salut, bienvenue dans ma super pyramide ! Combien d'étages veux-tu ?"
 print "> "
 num = gets.chomp.to_i
+if num < 1
+  num = 1
+elsif
+  num > 25
+  num = 25
+end
 
 puts "Voici la pyramide :"
 num.times do |iter|
--- exo_17.rb
+++ exo_17.rb
@@ -1,6 +1,13 @@
 puts "Salut, bienvenue dans ma super pyramide ! Combien d'étages veux-tu ?"
 print "> "
 num = gets.chomp.to_i
+if num < 1
+  num = 1
+elsif
+  num > 25
+  num = 25
+end
+
 spaces = " " 
 puts "Voici la pyramide :"
 num.times do |iter|
